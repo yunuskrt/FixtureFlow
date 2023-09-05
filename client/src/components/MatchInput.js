d39d4ff5151eba25be0e3679cd6856b1febbd8f2
Yunus <ykerestecioglu@sabanciuniv.edu>
2023-09-05 10:54:47 +0300

    league page backend connection & dynamic match input functionality

--- client/src/components/MatchInput.js
+++ client/src/components/MatchInput.js
@@ -1,15 +1,83 @@
-import React, { useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import '../styles/components/matchinput.css'
-const MatchInput = ({ match }) => {
-	const homeScoreRef = useRef()
-	const awayScoreRef = useRef()
+const MatchInput = ({ match, handleInputChange }) => {
+	const homeScoreRef = useRef(null)
+	const awayScoreRef = useRef(null)
+	const [matchData, setMatchData] = useState(match)
+	const [inputValidation, setInputValidation] = useState({
+		show: false,
+		message: '',
+	})
+	const handleInputValidation = () => {
+		const homeScoreValue = homeScoreRef.current.value
+		const awayScoreValue = awayScoreRef.current.value
+
+		if (homeScoreValue === '' && awayScoreValue === '') {
+			setInputValidation({ show: true, message: 'Fill both input fields.' })
+		} else if (homeScoreValue === '') {
+			setInputValidation({ show: true, message: 'Fill home score field.' })
+		} else if (awayScoreValue === '') {
+			setInputValidation({ show: true, message: 'Fill away score field.' })
+		} else if (homeScoreValue.includes('.') || awayScoreValue.includes('.')) {
+			setInputValidation({
+				show: true,
+				message: 'Score fields must be integer.',
+			})
+		} else if (homeScoreValue.includes('-') || awayScoreValue.includes('-')) {
+			setInputValidation({
+				show: true,
+				message: 'Score fields can not be less than zero.',
+			})
+		} else {
+			setInputValidation({ show: false, message: '' })
+		}
+	}
 	return (
-		<div className='matchContainer'>
-			<h5 className='teamNameLabel'>{match.homeTeam}</h5>
-			<input type='number' className='teamScoreInput' ref={homeScoreRef} />
-			-
-			<input type='number' className='teamScoreInput' ref={awayScoreRef} />
-			<h5 className='teamNameLabel'>{match.awayTeam}</h5>
+		<div className='matchWrapper'>
+			<div className='matchContainer'>
+				<h5 className='teamNameLabel'>{match.homeTeam}</h5>
+				<input
+					type='number'
+					className='teamScoreInput'
+					ref={homeScoreRef}
+					defaultValue={match.homeScore === null ? '' : match.homeScore}
+					onChange={() => {
+						setMatchData({
+							...matchData,
+							homeScore: homeScoreRef.current.value,
+						})
+						handleInputChange({
+							...matchData,
+							homeScore: homeScoreRef.current.value,
+						})
+						handleInputValidation()
+					}}
+					min={0}
+				/>
+				-
+				<input
+					type='number'
+					className='teamScoreInput'
+					ref={awayScoreRef}
+					defaultValue={match.awayScore === null ? '' : match.awayScore}
+					onChange={() => {
+						setMatchData({
+							...matchData,
+							awayScore: awayScoreRef.current.value,
+						})
+						handleInputChange({
+							...matchData,
+							awayScore: awayScoreRef.current.value,
+						})
+						handleInputValidation()
+					}}
+					min={0}
+				/>
+				<h5 className='teamNameLabel'>{match.awayTeam}</h5>
+			</div>
+			{inputValidation.show && (
+				<div className='invalidInput'>{inputValidation.message}</div>
+			)}
 		</div>
 	)
 }
--- client/src/pages/League.js
+++ client/src/pages/League.js
@@ -123,152 +123,182 @@ const League = () => {
 			],
 		},
 	]
-	const getMatchesForRound = (roundNumber) => {
-		const roundMatch = denemeMatches.find(
-			(match) => match.round === roundNumber
-		)
+
+	const { id } = useParams()
+	const [leagueData, setLeagueData] = useState(null)
+	const getMatchesForRound = (matches, roundNumber) => {
+		const roundMatch = matches.find((match) => match.round === roundNumber)
 		if (roundMatch) {
 			return [...roundMatch.matches]
 		} else {
 			return []
 		}
 	}
-	const { id } = useParams()
-	const [leagueData, setLeagueData] = useState(null)
+	const [paginationContent, setPaginationContent] = useState([])
+	const [updatedMatches, setUpdatedMatches] = useState([])
+	const handleMatchChange = (newMatch) => {
+		const index = updatedMatches.findIndex(
+			(match) => match._id === newMatch._id
+		)
+		if (index === -1) {
+			updatedMatches.push(newMatch)
+			setUpdatedMatches(updatedMatches)
+		} else {
+			updatedMatches[index] = newMatch
+			setUpdatedMatches(updatedMatches)
+		}
+	}
 	useEffect(() => {
 		async function fetchData() {
-			axios.get(`http://localhost:5000/api/leagues/${id}`).then((response) => {
-				if (response.status === 200) {
-					setLeagueData(response.data)
-				} else {
-					console.log('Invalid Page.')
-				}
-			})
+			await axios
+				.get(`http://localhost:5000/api/leagues/${id}`)
+				.then((response) => {
+					if (response.status === 200) {
+						setLeagueData(response.data)
+						setPaginationContent(getMatchesForRound(response.data.fixture, 1))
+					} else {
+						console.log('Invalid Page.')
+					}
+				})
 		}
 		fetchData()
 	}, [id])
 
-	const [paginationContent, setPaginationContent] = useState(
-		getMatchesForRound(1)
-	)
-
 	return (
 		<div style={{ width: '100%' }}>
-			<LeagueTitle
-				league={{
-					title: 'LeagueTitle',
-					winPoint: 3,
-					drawPoint: 1,
-					losePoint: 0,
-					rematch: true,
-					createdAt: '23 April 2022',
-				}}
-			/>
-			<Header title='Standings' />
-			<div className='ptable'>
-				<table>
-					<tr className='col'>
-						<th>#</th>
-						<th>Team</th>
-						<th>GP</th>
-						<th>W</th>
-						<th>D</th>
-						<th>L</th>
-						<th>PTS</th>
-					</tr>
-					<tr className='pos'>
-						<td>1</td>
-						<td>Teajsjksd 1</td>
-						<td>2</td>
-						<td>2</td>
-						<td>0</td>
-						<td>0</td>
-						<td>6</td>
-					</tr>
-					<tr className='pos'>
-						<td>2</td>
-						<td>Team 2</td>
-						<td>2</td>
-						<td>2</td>
-						<td>0</td>
-						<td>0</td>
-						<td>6</td>
-					</tr>
-					<tr className='pos'>
-						<td>3</td>
-						<td>Team 3</td>
-						<td>2</td>
-						<td>1</td>
-						<td>1</td>
-						<td>0</td>
-						<td>4</td>
-					</tr>
-					<tr className='pos'>
-						<td>4</td>
-						<td>Team 4</td>
-						<td>2</td>
-						<td>1</td>
-						<td>1</td>
-						<td>0</td>
-						<td>4</td>
-					</tr>
-					<tr className='pos'>
-						<td>5</td>
-						<td>Team 5</td>
-						<td>2</td>
-						<td>1</td>
-						<td>0</td>
-						<td>1</td>
-						<td>3</td>
-					</tr>
-					<tr className='pos'>
-						<td>6</td>
-						<td>Team 6</td>
-						<td>3</td>
-						<td>1</td>
-						<td>0</td>
-						<td>2</td>
-						<td>3</td>
-					</tr>
-					<tr className='pos'>
-						<td>7</td>
-						<td>Team 7</td>
-						<td>2</td>
-						<td>0</td>
-						<td>0</td>
-						<td>2</td>
-						<td>0</td>
-					</tr>
-				</table>
-			</div>
-			<Header title='Fixture' />
-			{/* <pre>{JSON.stringify(leagueData, null, 2)}</pre> */}
-			<div className='bottomWrapper'>
-				<div className='matchInputContainer'>
-					{paginationContent.map((match) => {
-						return (
-							<MatchInput
-								match={{ homeTeam: match.homeTeam, awayTeam: match.awayTeam }}
-								key={match._id}
-							/>
-						)
-					})}
-				</div>
-				{/* <pre>{JSON.stringify(paginationContent, null, 2)}</pre> */}
-				<IxButton class='m-1' variant='Primary'>
-					<IxIcon name='namur-ok-filled'></IxIcon>Save Changes
-				</IxButton>
-				<div className='paginationWrapper'>
-					<span className='roundLabel'>Round:</span>
-					<IxPagination
-						count={denemeMatches.length}
-						onPageSelected={(num) => {
-							console.log(num.detail)
-							setPaginationContent(getMatchesForRound(num.detail + 1))
+			{leagueData === null ? (
+				<div>loading</div>
+			) : (
+				<div>
+					<LeagueTitle
+						league={{
+							title: leagueData.name,
+							winPoint: leagueData.winPoint,
+							drawPoint: leagueData.drawPoint,
+							losePoint: leagueData.losePoint,
+							rematch: leagueData.rematch,
+							createdAt: leagueData.createdAt,
 						}}
 					/>
+					<Header title='Standings' />
+					<div className='ptable'>
+						<table>
+							<tr className='col'>
+								<th>#</th>
+								<th>Team</th>
+								<th>GP</th>
+								<th>W</th>
+								<th>D</th>
+								<th>L</th>
+								<th>PTS</th>
+							</tr>
+							<tr className='pos'>
+								<td>1</td>
+								<td>Teajsjksd 1</td>
+								<td>2</td>
+								<td>2</td>
+								<td>0</td>
+								<td>0</td>
+								<td>6</td>
+							</tr>
+							<tr className='pos'>
+								<td>2</td>
+								<td>Team 2</td>
+								<td>2</td>
+								<td>2</td>
+								<td>0</td>
+								<td>0</td>
+								<td>6</td>
+							</tr>
+							<tr className='pos'>
+								<td>3</td>
+								<td>Team 3</td>
+								<td>2</td>
+								<td>1</td>
+								<td>1</td>
+								<td>0</td>
+								<td>4</td>
+							</tr>
+							<tr className='pos'>
+								<td>4</td>
+								<td>Team 4</td>
+								<td>2</td>
+								<td>1</td>
+								<td>1</td>
+								<td>0</td>
+								<td>4</td>
+							</tr>
+							<tr className='pos'>
+								<td>5</td>
+								<td>Team 5</td>
+								<td>2</td>
+								<td>1</td>
+								<td>0</td>
+								<td>1</td>
+								<td>3</td>
+							</tr>
+							<tr className='pos'>
+								<td>6</td>
+								<td>Team 6</td>
+								<td>3</td>
+								<td>1</td>
+								<td>0</td>
+								<td>2</td>
+								<td>3</td>
+							</tr>
+							<tr className='pos'>
+								<td>7</td>
+								<td>Team 7</td>
+								<td>2</td>
+								<td>0</td>
+								<td>0</td>
+								<td>2</td>
+								<td>0</td>
+							</tr>
+						</table>
+					</div>
+					<Header title='Fixture' />
+					<pre>{JSON.stringify(leagueData, null, 2)}</pre>
+					<div className='bottomWrapper'>
+						<div className='matchInputContainer'>
+							{paginationContent.map((match) => {
+								return (
+									<MatchInput
+										match={{
+											_id: match._id,
+											homeTeam: match.homeTeam,
+											awayTeam: match.awayTeam,
+											homeScore: match.homeScore,
+											awayScore: match.awayScore,
+										}}
+										handleInputChange={(matchData) => {
+											console.log(matchData)
+											handleMatchChange(matchData)
+										}}
+										key={match._id}
+									/>
+								)
+							})}
+						</div>
+						{/* <pre>{JSON.stringify(paginationContent, null, 2)}</pre> */}
+						<IxButton class='m-1' variant='Primary'>
+							<IxIcon name='namur-ok-filled'></IxIcon>Save Changes
+						</IxButton>
+						<div className='paginationWrapper'>
+							<span className='roundLabel'>Round:</span>
+							<IxPagination
+								count={denemeMatches.length}
+								onPageSelected={(num) => {
+									setUpdatedMatches([])
+									setPaginationContent(
+										getMatchesForRound(leagueData.fixture, num.detail + 1)
+									)
+								}}
+							/>
+						</div>
+					</div>
 				</div>
-			</div>
+			)}
 		</div>
 	)
 }
